004ab02420f58fbd69322b0c3b58cf3e87fe988b
Deepan234 <80026698+Deepan234@users.noreply.github.com>
2022-08-31 00:27:12 +0530

    Update About.js

--- src/PortfolioContainer/AboutMe/About.js
+++ src/PortfolioContainer/AboutMe/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./About.css";
-import aboutImg from "./img/about.jpeg";
+import aboutImg from "./img/About1.PNG";
 function About() {
     //  Up To Top Btn
     window.addEventListener("scroll", function () {
